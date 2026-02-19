503ce943859cf0e8cda707dbfc9abf3dc6a9543d
Jekaterina <yeka@inbox.lv>
2026-02-19 23:56:44 +0200

    feat: add type explorer with truthy/falsy examples 2

--- type_explorer.py
+++ type_explorer.py
@@ -1,1 +1,21 @@
 #  2 pamata tipi vērtības mainīgajiem
+# str (teksts)
+name = "Jekaterina"
+surname = "Minčenko"
+print(type(surname))
+# int (veseli skaitļi)
+age = 33
+amount = 2
+print(type(age))
+# float (decimālskaitļi)
+x = 1.59
+y = 2003.5
+print(type(y))
+# bool (loģiskās vērtības)
+i_am_woman = True
+i_am_man = False
+print(type(i_am_man))
+# None (nav vērtības)
+time = None
+code = None
+print(type(code))
